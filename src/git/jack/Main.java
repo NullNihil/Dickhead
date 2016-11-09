0a4c75b8862426098a905a9da76a42b7fdb542fc
NullNihil <ca3q6pvd1e@libero.it>
2016-11-09 21:59:36 +0100

    OS checking code added.

--- src/git/jack/Main.java
+++ src/git/jack/Main.java
@@ -1,17 +1,33 @@
 package git.jack;
 
-import java.io.IOException;
-
 public class Main{
 
 	public static final Runtime runtime = Runtime.getRuntime();
 
 	public static void main(String args[]){
-		String command = "cmd echo hi";
-		try{
-			System.out.println(runtime.exec(command));
-		}catch(IOException e){
-			e.printStackTrace();
+		//PROCESS EXAMPLE
+		//		try{
+		//			String command = "cmd /C set";
+		//			Process process = null;
+		//
+		//			process = runtime.exec(command);
+		//			StreamReader reader = new StreamReader(process.getInputStream());
+		//
+		//			reader.start();
+		//			process.waitFor();
+		//			reader.join();
+		//
+		//			String result = reader.getResult();
+		//			System.out.println(result);
+		//		}catch(Exception e){
+		//			e.printStackTrace();
+		//		}
+		String osName = System.getenv("OS");
+		if(osName.toLowerCase().contains("windows")){
+			System.out.println("WINDOWS DETECTED");
+		}
+		if(System.getProperty("os.name").toString().toLowerCase().contains("mac")){
+			System.out.println("BUCCHERI DETECTED");
 		}
 	}
 }
