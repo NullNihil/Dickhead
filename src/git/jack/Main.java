633a39752c9a2917612ee8c202602834f0da6d0d
NullNihil <ca3q6pvd1e@libero.it>
2016-11-21 19:26:21 +0100

    +Esempio

--- src/git/jack/Main.java
+++ src/git/jack/Main.java
@@ -5,23 +5,22 @@ public class Main{
 	public static final Runtime runtime = Runtime.getRuntime();
 
 	public static void main(String args[]){
-		//PROCESS EXAMPLE
-		//		try{
-		//			String command = "cmd /C set";
-		//			Process process = null;
-		//
-		//			process = runtime.exec(command);
-		//			StreamReader reader = new StreamReader(process.getInputStream());
-		//
-		//			reader.start();
-		//			process.waitFor();
-		//			reader.join();
-		//
-		//			String result = reader.getResult();
-		//			System.out.println(result);
-		//		}catch(Exception e){
-		//			e.printStackTrace();
-		//		}
+		try{
+			String command = "cmd /C set";
+			Process process = null;
+
+			process = runtime.exec(command);
+			StreamReader reader = new StreamReader(process.getInputStream());
+
+			reader.start();
+			process.waitFor();
+			reader.join();
+
+			String result = reader.getResult();
+			System.out.println(result);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 
 		//metti nome di os in osName, vedi se contiene "windows", se sì allora è windows.
 		String osName = System.getenv("OS");
