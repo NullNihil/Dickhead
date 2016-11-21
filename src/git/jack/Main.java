823029462f62c4b2b2a5142a5a1b14d14af33558
NullNihil <ca3q6pvd1e@libero.it>
2016-11-21 20:09:47 +0100

    +Commenti +Variabili interessanti

--- src/git/jack/Main.java
+++ src/git/jack/Main.java
@@ -5,17 +5,27 @@ public class Main{
 	public static final Runtime runtime = Runtime.getRuntime();
 
 	public static void main(String args[]){
+		/*
+		 * cose interessanti scritte in ordine di comparsa nel comando ALLUSERPROFILE APPDATA ComSpec HOMEDRIVE HOMEPATH SystemDrive USERNAME USERPROFILE windir
+		 */
 		try{
+			//crea string con comando e processo vuoto
 			String command = "cmd /C set";
 			Process process = null;
 
+			//esegue il comando col processo
 			process = runtime.exec(command);
+			//crea un leggi-processo
 			StreamReader reader = new StreamReader(process.getInputStream());
 
+			//apre il leggi-processo
 			reader.start();
+			//aspetta che finisce il processo
 			process.waitFor();
+			//chiude il leggi-processo
 			reader.join();
 
+			//prende risultato
 			String result = reader.getResult();
 			System.out.println(result);
 		}catch(Exception e){
@@ -26,6 +36,10 @@ public class Main{
 		String osName = System.getenv("OS");
 		if(osName.toLowerCase().contains("windows")){
 			System.out.println("WINDOWS DETECTED");
+		}else if(osName.toLowerCase().contains("mac")){
+			System.out.println("MAC DETECTED");
+		}else if(osName.toLowerCase().contains("linux")){
+			System.out.println("LINUX DETECTED");
 		}
 	}
 }
