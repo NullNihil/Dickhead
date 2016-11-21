9b8270f09257c262ccca39e372b681e760aac05c
NullNihil <ca3q6pvd1e@libero.it>
2016-11-21 20:30:44 +0100

    +{} in StreamReader:run():while()

--- src/git/jack/StreamReader.java
+++ src/git/jack/StreamReader.java
@@ -18,8 +18,9 @@ public class StreamReader extends Thread{
 	public void run(){
 		try{
 			int c;
-			while((c = is.read()) != -1)
+			while((c = is.read()) != -1){
 				sw.write(c);
+			}
 		}catch(IOException e){
 			e.printStackTrace();
 		}
